840dcab9adcee94ca72a62c0669f97f4131fb679
ItsWaterBoi <itswaterboi@gmail.com>
2023-12-08 19:27:41 -0500

    aaaaaaaaa

--- base/conn.php
+++ base/conn.php
@@ -2,6 +2,6 @@
 
 <?php 
 
-$pdo = new PDO("mysql:host=localhost; user=wathdgames; dbname=wathdgames; password=jxXxMPDnaNZDijH6; charset=utf8mb4");
+$pdo = new PDO("mysql:host=host; user=username; dbname=username; password=password; charset=utf8mb4");
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
